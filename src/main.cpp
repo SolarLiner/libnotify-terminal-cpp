7e05471067ca2b319e464823502b80eee2bafeeb
Nathan Graule <solarliner@gmail.com>
2017-12-27 19:08:51 +0100

    Changed args from char* to std::string

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,36 @@
 #include <iostream>
+#include <string>
+#include <vector>
+#include <tuple>
 #include "../lib/args/args.hxx"
+#include "Notification.hpp"
 
-
-void help()
+int main(int argc, const char** argv)
 {
-    std::cout << "libnotify-terminal is a small program that interfaces ";
-    std::cout << "with libnotify, allowing you to send notifications from ";
-    std::cout << "commandline. Contrary to notify-send, this supports ";
-    std::cout << "callbacks, allowing you to place buttons." << std::endl;
-}
-
-int main(int argc, char **argv)
-{
-    args::ArgumentParser parser("Short text #1", "This goes after the options");
+    args::ArgumentParser parser("Display notifications from command-line, with callbacks.");
     args::HelpFlag help(parser, "help", "Displays this help menu", {'h', "help"});
+
+    args::Group content(parser, "Add content to your notification", args::Group::Validators::DontCare);
+    args::Positional<std::string> body(content, "body", "Notification body", args::Options::Required);
+    args::ValueFlag<std::string> title(content, "title", "Notification title", {'t', "title"});
+    args::ValueFlag<std::string> icon(content, "icon", "freedesktop icon name", {"icon"}, args::Options::Single);
+
+    args::Group config(parser, "Configuration options", args::Group::Validators::DontCare);
+    args::ValueFlag<std::string> appname(config, "Application name", {"appname"}, args::Options::Required);
+    args::ValueFlag<int> timeout(config, "Notification timeout. Only applies to DEs that support it.", {"timeout"}, args::Options::)
+
+    args::Group buttons(parser, "Add buttons to the notification (if the desktop environment supports it)", args::Group::Validators::DontCare);
+    args::ValueFlag<std::string> button(buttons, "button", "Adds a button by its action callback and user label", {"button"});
+
+    parser.LongSeparator(" ");
 
     try
     {
-        parser.ParseCLI(argc, argv);
+        std::vector<std::string> args(argv+1, argv+argc);
+        parser.Prog(argv[0]);
+        parser.ParseArgs(args);
+
+        //parser.ParseCLI(argc, argv);
     }
     catch (args::Help h)
     {
@@ -30,6 +43,18 @@ int main(int argc, char **argv)
         std::cerr << parser;
         return 1;
     }
+    catch(args::ValidationError e)
+    {
+        std::cerr << e.what() << std::endl;
+        std::cerr << parser;
+        return 1;
+    }
 
-    return 0;
+    Notification n(args::get(appname), args::get(title), args::get(body));
+    n.set_timeout(5000);
+
+    if(n.show()) return 0;
+
+    std::cerr << "Could not display notification." << std::endl;
+    return -1;
 }
