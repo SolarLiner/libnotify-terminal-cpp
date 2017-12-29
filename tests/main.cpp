fde95702705702da0e69313a12900a75521d0172
Nathan Graule <solarliner@gmail.com>
2017-12-29 22:42:53 +0100

    Added mininal tests

--- tests/main.cpp
+++ tests/main.cpp
@@ -3,6 +3,14 @@
 #include "../src/Notification.hpp"
 
 TEST_CASE("a Notification object is being created", "[Notification]") {
+	REQUIRE_NOTHROW([&]() {
+		Notification n("Application", "Title", "Body");
+		n.set_timeout(5e3);
+		n.show();
+	});
+}
+
+TEST_CASE("Notifiction shows successfully", "[Notification]") {
 	Notification n("Application", "Title", "Body");
-	REQUIRE(n != null);
+	REQUIRE(n.show() == true);
 }
